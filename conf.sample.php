d404c52fc98772c7a289b869bbb97ab9236797f0
Augusto Beiro <abeiro@crsur.es>
2023-05-10 07:34:23 +0200

    * Updated conf.sample.php

--- conf.sample.php
+++ conf.sample.php
@@ -9,15 +9,24 @@ $DEBUG_MODE=false;
 $PLAYER_NAME="Prisoner";
 $HERIKA_PERS="You are Herika,a breton female who likes jokes and sarcastic comments";
 
+// Azure TTS Stuff. Copy desired block to conf.php (only one block)
+// This conf is for variable mood
+$AZURETTS_CONF["fixedMood"]="";					// Azure TTS prosody and style. Empty to stay variable.
+$AZURETTS_CONF["region"]="westeurope";			// Region, Fine tune to improve reponse time. https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
+$AZURETTS_CONF["voice"]="en-US-JennyNeural";	// Voice. Read https://learn.microsoft.com/es-es/azure/cognitive-services/speech-service/language-support?tabs=tts
+$AZURETTS_CONF["volume"]="20";					// Default volume
+$AZURETTS_CONF["rate"]="1.25";					// Default rate (speed)
+$AZURETTS_CONF["countour"]="";					
 
-// Azure TTS stuff
+// This conf is for variable mood is mastered by rang97. 
+
 $AZURETTS_CONF["fixedMood"]="default";			// Azure TTS prosody and style. Empty to stay variable.
 $AZURETTS_CONF["region"]="westeurope";			// Region, Fine tune to improve reponse time. https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
 $AZURETTS_CONF["voice"]="en-US-NancyNeural";	// Voice. Read https://learn.microsoft.com/es-es/azure/cognitive-services/speech-service/language-support?tabs=tts
 $AZURETTS_CONF["volume"]="25";					// Default volume
 $AZURETTS_CONF["rate"]="1.2";					// Default rate (speed)
 $AZURETTS_CONF["countour"]=
-	"(11%, +15%) (60%, -23%) (80%, -34%)";		//			
+	"(11%, +15%) (60%, -23%) (80%, -34%)";		//				
 
 
 ?>
