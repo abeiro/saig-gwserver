7f18f859533d7edca625f96fd4a7faa204cc443a
Augusto Beiro <augusto.beiro@gmail.com>
2023-05-13 18:53:03 +0200

    * Updated conf sample

--- conf.sample.php
+++ conf.sample.php
@@ -18,7 +18,8 @@ $AZURETTS_CONF["volume"]="20";					// Default volume
 $AZURETTS_CONF["rate"]="1.25";					// Default rate (speed)
 $AZURETTS_CONF["countour"]="";					
 
-// This conf is for variable mood is mastered by rang97. 
+// Azure TTS Stuff. Copy desired block to conf.php (only one block)
+// This conf is for variable fixed is mastered by rang97. 
 
 $AZURETTS_CONF["fixedMood"]="default";			// Azure TTS prosody and style. Empty to stay variable.
 $AZURETTS_CONF["region"]="westeurope";			// Region, Fine tune to improve reponse time. https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
@@ -28,5 +29,9 @@ $AZURETTS_CONF["rate"]="1.2";					// Default rate (speed)
 $AZURETTS_CONF["countour"]=
 	"(11%, +15%) (60%, -23%) (80%, -34%)";		//				
 
-
+// If using MIMIC3, comment out $AZURE_API_KEY and uncomment $MIMIC3
+//$MIMIC3="http://127.0.0.1:59125";               // Don't set if using Azure.
+$MIMIC3_CONF["voice"]="en_US/ljspeech_low";
+$GLOBALS["MIMIC3_CONF"]["rate"]="1.25";
+$GLOBALS["MIMIC3_CONF"]["volume"]="80";
 ?>
