aaf54d51b73932624e8842b29618daf0477c7c34
Augusto Beiro <abeiro@crsur.es>
2023-05-17 06:57:58 +0200

    * Support for stt (Azure and Whisper)

--- stt.php
+++ stt.php
@@ -1,28 +1,25 @@
 <?php
 
-//file_put_contents(__DIR__.DIRECTORY_SEPARATOR."soundcache/test.wav",base64_decode($_POST["wavData"]));
+$path = dirname((__FILE__)) . DIRECTORY_SEPARATOR;
+require_once($path . "conf.php"); // API KEY must be there
 
 $finalName=__DIR__.DIRECTORY_SEPARATOR."soundcache/".md5($_FILES["file"]["tmp_name"]).".wav";
 
 @copy($_FILES["file"]["tmp_name"] ,$finalName);
 
 
-$path = dirname((__FILE__)) . DIRECTORY_SEPARATOR ;
-require_once($path . "conf.php");
-require_once($path . "vendor/autoload.php");
-require_once($path . "lib/$DRIVER.class.php");
-require_once($path . "lib/Misc.php");
+if ($STTFUNCTION=="azure") {
+    
+    require_once($path."stt/stt-azure.php");
+    return stt($finalName);
+    
+} else if ($STTFUNCTION=="whisper") { 
 
+    require_once($path."stt/stt-whisper.php");
+    return stt($finalName);
+    
+}
 
-$client = OpenAI::client($GLOBALS["OPENAI_API_KEY"]);
- 
-$response = $client->audio()->transcribe([
-    'model' => 'whisper-1',
-    'file' => fopen($finalName, 'r'),
-    'response_format' => 'verbose_json',
-]);
-
-echo $response["text"];
 
 ?>
 
---
+++ stt/stt-whisper.php
@@ -0,0 +1,23 @@
+<?php
+$path = dirname((__FILE__)) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR;
+require_once($path . "conf.php"); // API KEY must be there
+
+require_once($path . "vendor/autoload.php");
+require_once($path . "lib/$DRIVER.class.php");
+require_once($path . "lib/Misc.php");
+
+
+function sst($file) {
+  $client = OpenAI::client($GLOBALS["OPENAI_API_KEY"]);
+  
+  $response = $client->audio()->transcribe([
+      'model' => 'whisper-1',
+      'file' => fopen($file, 'r'),
+      'response_format' => 'verbose_json',
+  ]);
+
+  return $response["text"];
+
+}
+
+?>
