464ce890452c191e9fa5ce6689f90678f3ccdd23
RANGROO <reecemeakings@gmail.com>
2023-05-24 14:08:20 -0400

    Azure config changes

--- conf.sample.php
+++ conf.sample.php
@@ -7,28 +7,17 @@ $AZURE_API_KEY="";  //Azure Free: https://azure.microsoft.com/en-us/try/cognitiv
 
 $DEBUG_MODE=false;
 $PLAYER_NAME="Prisoner";
-$HERIKA_PERS="You are Herika,a breton female who likes jokes and sarcastic comments";
+$HERIKA_PERS="You are Herika, a Breton female who likes jokes and sarcastic comments.";
 
 // Azure TTS Stuff. Copy desired block to conf.php (only one block)
-// This conf is for variable mood
-$AZURETTS_CONF["fixedMood"]="";					// Azure TTS prosody and style. Empty to stay variable.
-$AZURETTS_CONF["region"]="westeurope";			// Region, Fine tune to improve reponse time. https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
-$AZURETTS_CONF["voice"]="en-US-JennyNeural";	// Voice. Read https://learn.microsoft.com/es-es/azure/cognitive-services/speech-service/language-support?tabs=tts
-$AZURETTS_CONF["volume"]="20";					// Default volume
-$AZURETTS_CONF["rate"]="1.25";					// Default rate (speed)
-$AZURETTS_CONF["countour"]="";					
-$AZURETTS_CONF["validMoods"]=array("whispering","default");	// New, limits moods allowed in TTS transcription
-
-// Azure TTS Stuff. Copy desired block to conf.php (only one block)
-// This conf is for variable fixed is mastered by rang97. 
-
-$AZURETTS_CONF["fixedMood"]="default";			// Azure TTS prosody and style. Empty to stay variable.
+// More Azure TTS presets can be found in the Article section for the mod page
+// Setting "fixedMood" will force Herika to speak with a single mood/tone
+$AZURETTS_CONF["fixedMood"]="";			// Azure TTS prosody and style. Empty to stay variable.
 $AZURETTS_CONF["region"]="westeurope";			// Region, Fine tune to improve reponse time. https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
 $AZURETTS_CONF["voice"]="en-US-NancyNeural";	// Voice. Read https://learn.microsoft.com/es-es/azure/cognitive-services/speech-service/language-support?tabs=tts
 $AZURETTS_CONF["volume"]="25";					// Default volume
 $AZURETTS_CONF["rate"]="1.2";					// Default rate (speed)
-$AZURETTS_CONF["countour"]=
-	"(11%, +15%) (60%, -23%) (80%, -34%)";		//				
+$AZURETTS_CONF["countour"]= "(11%, +15%) (60%, -23%) (80%, -34%)";		//				
 
 // If using MIMIC3, comment out $AZURE_API_KEY and uncomment $MIMIC3
 //$MIMIC3="http://127.0.0.1:59125";               // Don't set if using Azure.
