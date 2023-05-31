bedeef7fe58ab12ad13e2dbba842ccd1f5f00f8d
Augusto Beiro <abeiro@crsur.es>
2023-05-31 07:19:48 +0200

    * Check for data in STT

--- stt.php
+++ stt.php
@@ -5,6 +5,10 @@ require_once($path . "conf.php"); // API KEY must be there
 
 $finalName=__DIR__.DIRECTORY_SEPARATOR."soundcache/_stt_".md5($_FILES["file"]["tmp_name"]).".wav";
 
+
+if (!$_FILES["file"]["tmp_name"])
+    die("STT error, no data given");
+
 @copy($_FILES["file"]["tmp_name"] ,$finalName);
 
 
