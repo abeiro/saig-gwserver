2e24fae3d60772cbb0a723e5ffcb6d735b1b46a7
Augusto Beiro <abeiro@crsur.es>
2023-07-26 16:47:15 +0200

    * Changed whisper stt, removed openai-php dependency. Raw REST API in use now.

--- stt/stt-whisper.php
+++ stt/stt-whisper.php
@@ -2,25 +2,53 @@
 $path = dirname((__FILE__)) . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR;
 require_once($path . "conf.php"); // API KEY must be there
 
-require_once($path . "vendor/autoload.php");
-require_once($path . "lib/$DRIVER.class.php");
-require_once($path . "lib/Misc.php");
+function stt($file)
+{
+    $url = "https://api.openai.com/v1/audio/transcriptions";
+    
+    $filePath = $file;
+    $boundary = '----WebKitFormBoundary' . md5(mt_rand() . microtime());
+    $contentType = 'multipart/form-data; boundary=' . $boundary;
+
+    // Prepare the file content
+    $fileContent = file_get_contents($filePath);
+    $filename = basename($filePath);
+    $multipartBody = "--{$boundary}\r\n"
+        ."Content-Disposition: form-data; name=\"file\"; filename=\"{$filename}\"\r\n"
+        ."Content-Type: audio/wav\r\n"
+        ."Content-Transfer-Encoding: binary\r\n\r\n"
+        .$fileContent . "\r\n"
+        ."--{$boundary}\r\n"
+        ."Content-Disposition: form-data; name=\"model\"\r\n\r\n"
+        ."whisper-1\r\n"
+        ."--{$boundary}\r\n"
+        ."Content-Disposition: form-data; name=\"prompt\"\r\n\r\n"
+        ."{$GLOBALS["HERIKA_NAME"]},Dragonborn,Whiterun\r\n"
+        ."--{$boundary}\r\n"
+        ."Content-Disposition: form-data; name=\"language\"\r\n\r\n"
+        ."{$GLOBALS["TTSLANGUAGE_WHISPER"]}\r\n"
+        ."--{$boundary}--\r\n";
+         
+    $contextOptions = [
+        'http' => [
+            'method' => 'POST',
+            'header' => "Authorization: Bearer  {$GLOBALS["OPENAI_API_KEY"]}\r\n"."Content-Type: {$contentType}\r\n" .
+            "Content-Length: " . strlen($multipartBody) . "\r\n",
+            'content' => $multipartBody,
+        ],
+    ];
+
+    $context = stream_context_create($contextOptions);
+    $response = file_get_contents($url, false, $context);
 
 
-function stt($file) {
-  $client = OpenAI::client($GLOBALS["OPENAI_API_KEY"]);
-  $lang=($GLOBALS["TTSLANGUAGE_WHISPER"])?$GLOBALS["TTSLANGUAGE_WHISPER"]:"en";
+    if ($response === false) {
 
-  $response = $client->audio()->transcribe([
-      'model' => 'whisper-1',
-      'file' => fopen($file, 'r'),
-      'response_format' => 'verbose_json',
-      'language'=>$GLOBALS["TTSLANGUAGE_WHISPER"],
-      'prompt'=>"{$GLOBALS["HERIKA_NAME"]}, the Dragonborns are taking the Nords to Whiterun! Check that bard\'s lute! Fus Ro Dah!"
-  ]);
+    } else {
 
-  return $response["text"];
 
+    }
+    $reponseParsed = json_decode($response);
+
+    return $reponseParsed->text;
 }
-
-?>
